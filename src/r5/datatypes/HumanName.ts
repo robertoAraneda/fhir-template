4dbee6b8991c446b044c0b21bda9e25f64ddb924
Roberto Araneda Espinoza <robaraneda@gmail.com>
2023-05-25 21:02:14 -0400

    add humanname

--- src/r5/datatypes/HumanName.ts
+++ src/r5/datatypes/HumanName.ts
@@ -9,3 +9,97 @@ export class HumanName {
   suffix?: string[];
   period?: Period;
 }
+
+export class HumanNameBuilder {
+  private use: string;
+  private text: string;
+  private family: string;
+  private given: string[];
+  private prefix: string[];
+  private suffix: string[];
+  private period: Period;
+
+  getUse(): string {
+    return this.use;
+  }
+
+  setUse(value: string): HumanNameBuilder {
+    this.use = value;
+
+    return this;
+  }
+
+  getText(): string {
+    return this.text;
+  }
+
+  setText(value: string): HumanNameBuilder {
+    this.text = value;
+
+    return this;
+  }
+
+  getFamily(): string {
+    return this.family;
+  }
+
+  setFamily(value: string): HumanNameBuilder {
+    this.family = value;
+
+    return this;
+  }
+
+  getGiven(): string[] {
+    return this.given;
+  }
+
+  setGiven(value: string[]): HumanNameBuilder {
+    this.given = value;
+
+    return this;
+  }
+
+  getPrefix(): string[] {
+    return this.prefix;
+  }
+
+  setPrefix(value: string[]): HumanNameBuilder {
+    this.prefix = value;
+
+    return this;
+  }
+
+  getSuffix(): string[] {
+    return this.suffix;
+  }
+
+  setSuffix(value: string[]): HumanNameBuilder {
+    this.suffix = value;
+
+    return this;
+  }
+
+  getPeriod(): Period {
+    return this.period;
+  }
+
+  setPeriod(value: Period): HumanNameBuilder {
+    this.period = value;
+
+    return this;
+  }
+
+  build(): HumanName {
+    const humanName = new HumanName();
+
+    humanName.use = this.use;
+    humanName.text = this.text;
+    humanName.family = this.family;
+    humanName.given = this.given;
+    humanName.prefix = this.prefix;
+    humanName.suffix = this.suffix;
+    humanName.period = this.period;
+
+    return humanName;
+  }
+}
--- src/r5/resources/Patient.ts
+++ src/r5/resources/Patient.ts
@@ -4,13 +4,63 @@ import { ContactPoint } from '../datatypes/ContactPoint';
 import { HumanName } from '../datatypes/HumanName';
 import { Identifier } from '../datatypes/Identifier';
 
-export class PatientR5 extends ResourceR5 {
-  private identifiers: Identifier[];
+export interface PatientParams {
+  id?: string;
+  identifiers?: Identifier[];
   active?: boolean;
   name?: HumanName[];
   telecom?: ContactPoint[];
   gender?: AdministrativeGenderVS;
   birthDate?: string;
+}
+
+type SetIdentifier = Omit<ISetterPatient, 'SetIdentifier'>;
+type SetId = Omit<ISetterPatient, 'SetId'>;
+type SetIdentifiers = Omit<ISetterPatient, 'SetIdentifiers'>;
+type SetHumanName = Omit<ISetterPatient, 'SetHumanName'>;
+type SetHumanNames = Omit<ISetterPatient, 'SetHumanNames'>;
+
+export interface ISetterPatient {
+  setId(id: string): SetId;
+  setIdentifier(index: number, identifier: Identifier): SetIdentifier;
+  setIdentifiers(identifiers: Identifier[]): SetIdentifiers;
+  setHumanName(index: number, name: HumanName): SetHumanName;
+  setHumanNames(names: HumanName[]): SetHumanNames;
+}
+
+export class PatientR5 extends ResourceR5 {
+  private identifiers: Identifier[];
+  private active?: boolean;
+  private name?: HumanName[];
+  private telecom?: ContactPoint[];
+  private gender?: AdministrativeGenderVS;
+  private birthDate?: string;
+
+  setHumanName(index: number, name: HumanName) {
+    if (!this.name) this.name = [];
+    this.name[index] = name;
+
+    return this;
+  }
+
+  getHumanName(index: number) {
+    if (!this.name) return null;
+    return this.name[index];
+  }
+
+  getHumanNames() {
+    return this.name;
+  }
+
+  setActive(active: boolean) {
+    this.active = active;
+
+    return this;
+  }
+
+  getActive() {
+    return this.active;
+  }
 
   addIdentifier(identifier: Identifier) {
     if (!this.identifiers) this.identifiers = [];
@@ -48,9 +98,8 @@ export class PatientR5 extends ResourceR5 {
     return JSON.parse(json);
   }
 
-  constructor() {
+  constructor(args?: PatientParams) {
     super('Patient');
-
-    this.identifiers = [];
+    Object.assign(this, args);
   }
 }

a064e6fcb3d078dc12de343fd21f26102a4caa86
Roberto Araneda Espinoza <robaraneda@gmail.com>
2023-06-05 13:45:50 -0400

    create new file for r5 context

--- src/r5/index.ts
+++ src/r5/index.ts
@@ -1,4 +1,45 @@
-import { Wait } from './validators/BackboneElementValidator';
+import { BackboneElementValidator, Wait } from './validators/BackboneElementValidator';
+import { Endpoint, Organization, Patient, Person, Practitioner, PractitionerRole, RelatedPerson } from './resources';
+import {
+  createBackboneElementBuilderType,
+  createDatatypeBuilderType,
+  createResourceBuilderType,
+  createResourceType,
+} from '../helpers';
+import {
+  EndpointBuilder,
+  OrganizationBuilder,
+  PatientBuilder,
+  PersonBuilder,
+  PractitionerBuilder,
+  PractitionerRoleBuilder,
+  RelatedPersonBuilder,
+} from './builders/resources';
+import {
+  EndpointPayloadBuilder,
+  OrganizationQualificationBuilder,
+  PatientCommunicationBuilder,
+  PatientContactBuilder,
+  PatientLinkBuilder,
+  PersonCommunicationBuilder,
+  PersonLinkBuilder,
+  PractitionerCommunicationBuilder,
+  PractitionerQualificationBuilder,
+  RelatedPersonCommunicationBuilder,
+} from './builders/backbones';
+import {
+  AddressBuilder,
+  AttachmentBuilder,
+  CodeableConceptBuilder,
+  ContactPointBuilder,
+  HumanNameBuilder,
+  IdentifierBuilder,
+  MetaBuilder,
+  PeriodBuilder,
+  ReferenceBuilder,
+} from './builders/datatypes';
+import { DataTypesValidator } from './validators/ElementValidator';
+import { ResourceValidator } from './validators/ResourceValidator';
 
 export interface IBackboneValidatorProperties {
   EndpointPayload: (data: unknown) => Wait;
@@ -40,6 +81,165 @@ export interface IValidatorContext {
   backboneElements: IBackboneValidatorProperties;
   dataTypes: IDatatypeValidatorProperties;
   resources: IResourceValidatorProperties;
+}
+
+export type ResourceTypeR5 =
+  | 'Patient'
+  | 'Organization'
+  | 'Endpoint'
+  | 'Person'
+  | 'Practitioner'
+  | 'PractitionerRole'
+  | 'RelatedPerson';
+
+export type BackboneTypeR5 =
+  | 'PatientContact'
+  | 'PatientCommunication'
+  | 'PatientLink'
+  | 'PersonCommunication'
+  | 'PersonLink'
+  | 'PractitionerCommunication'
+  | 'PractitionerQualification'
+  | 'RelatedPersonCommunication'
+  | 'OrganizationQualification'
+  | 'EndpointPayload';
+
+export type DatatypeTypeR5 =
+  | 'Address'
+  | 'Attachment'
+  | 'CodeableConcept'
+  | 'Coding'
+  | 'ContactPoint'
+  | 'HumanName'
+  | 'Identifier'
+  | 'Meta'
+  | 'Period'
+  | 'Reference';
+
+export class FhirContextR5 {
+  /*
+  public async createResource<T extends IDomainResource>(
+    resourceType: ResourceType,
+    payload: T extends IDomainResource ? T : unknown,
+  ) {
+    const builder = await import(`./r5/resources/${resourceType}`);
+
+    return new builder[`${resourceType}`](payload) as T;
+  }
+
+
+
+  public async createResource(resourceType: ResourceType) {
+    const entityType = createResourceTypeInstance(resourceType);
+    if (!entityType) throw new Error(`Resource type ${resourceType} not found`);
+
+    const entity = await import(`./r5/resources/${resourceType}`);
+
+    return {
+      payload: (payload: typeof entityType) => new entity[`${resourceType}`](payload) as typeof entityType,
+    };
+  }
+
+   */
+
+  public createResource<T extends ResourceTypeR5>(resourceType: T) {
+    type ResourceInstance = T extends 'Patient'
+      ? Patient
+      : T extends 'Endpoint'
+      ? Endpoint
+      : T extends 'Person'
+      ? Person
+      : T extends 'Practitioner'
+      ? Practitioner
+      : T extends 'RelatedPerson'
+      ? RelatedPerson
+      : T extends 'Organization'
+      ? Organization
+      : T extends 'PractitionerRole'
+      ? PractitionerRole
+      : unknown;
+    return {
+      payload: (payload: ResourceInstance) => createResourceType<ResourceInstance>(resourceType, payload),
+    };
+  }
+
+  public async createResourceWithBuilder<T extends ResourceTypeR5>(resourceType: ResourceTypeR5) {
+    type ResourceInstance = T extends 'Patient'
+      ? PatientBuilder
+      : T extends 'Endpoint'
+      ? EndpointBuilder
+      : T extends 'Person'
+      ? PersonBuilder
+      : T extends 'Practitioner'
+      ? PractitionerBuilder
+      : T extends 'RelatedPerson'
+      ? RelatedPersonBuilder
+      : T extends 'Organization'
+      ? OrganizationBuilder
+      : T extends 'PractitionerRole'
+      ? PractitionerRoleBuilder
+      : unknown;
+
+    return createResourceBuilderType<ResourceInstance>(resourceType);
+  }
+
+  public async createBackboneElementWithBuilder<T extends BackboneTypeR5>(resourceType: BackboneTypeR5) {
+    type ResourceInstance = T extends 'PatientContact'
+      ? PatientContactBuilder
+      : T extends 'PatientCommunication'
+      ? PatientCommunicationBuilder
+      : T extends 'PatientLink'
+      ? PatientLinkBuilder
+      : T extends 'PersonCommunication'
+      ? PersonCommunicationBuilder
+      : T extends 'PersonLink'
+      ? PersonLinkBuilder
+      : T extends 'PractitionerCommunication'
+      ? PractitionerCommunicationBuilder
+      : T extends 'PractitionerQualification'
+      ? PractitionerQualificationBuilder
+      : T extends 'RelatedPersonCommunication'
+      ? RelatedPersonCommunicationBuilder
+      : T extends 'OrganizationQualification'
+      ? OrganizationQualificationBuilder
+      : T extends 'EndpointPayload'
+      ? EndpointPayloadBuilder
+      : unknown;
+
+    return createBackboneElementBuilderType<ResourceInstance>(resourceType);
+  }
+
+  public async createDatatypeWithBuilder<T extends DatatypeTypeR5>(resourceType: DatatypeTypeR5) {
+    type ResourceInstance = T extends 'Address'
+      ? AddressBuilder
+      : T extends 'Attachment'
+      ? AttachmentBuilder
+      : T extends 'CodeableConcept'
+      ? CodeableConceptBuilder
+      : T extends 'ContactPoint'
+      ? ContactPointBuilder
+      : T extends 'HumanName'
+      ? HumanNameBuilder
+      : T extends 'Identifier'
+      ? IdentifierBuilder
+      : T extends 'Meta'
+      ? MetaBuilder
+      : T extends 'Period'
+      ? PeriodBuilder
+      : T extends 'Reference'
+      ? ReferenceBuilder
+      : unknown;
+
+    return createDatatypeBuilderType<ResourceInstance>(resourceType);
+  }
+
+  public getValidator(): IValidatorContext {
+    return {
+      backboneElements: BackboneElementValidator,
+      dataTypes: DataTypesValidator,
+      resources: ResourceValidator,
+    };
+  }
 }
 
 /*

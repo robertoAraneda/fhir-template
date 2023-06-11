fbedc151b989ef71c50c4e4053a4980f54c4e892
Roberto Araneda Espinoza <robaraneda@gmail.com>
2023-06-11 18:37:16 -0400

    added unit test PractitionerQualification R4

--- tests/r4/backbones/PractitionerQualification.spec.ts
+++ tests/r4/backbones/PractitionerQualification.spec.ts
@@ -1,142 +1,192 @@
+import FHIRContext from '../../../src';
+import { PractitionerQualification } from '../../../src/r4/models/backbones/PractitionerQualification';
 import { PractitionerQualificationBuilder } from '../../../src/r4/builders/backbones';
 import { IPractitionerQualification } from '../../../src/r4/interfaces/backbones';
-import { IValidatorContext } from '../../../src/r4';
-import FHIRContext from '../../../src';
 
-describe('PractitionerQualification', () => {
-  let validator: IValidatorContext;
+describe('PractitionerQualification FHIR R4', () => {
   let builder: PractitionerQualificationBuilder;
-
-  beforeAll(() => {
-    const context = new FHIRContext();
-    validator = context.forR4().validators;
-  });
+  let builderFromFunction: PractitionerQualificationBuilder;
+  const { Validator, createBackboneElement, Builder } = new FHIRContext().forR4();
 
   // create global
   beforeEach(() => {
     builder = new PractitionerQualificationBuilder();
+    builderFromFunction = Builder.backboneElements.PractitionerQualificationBuilder();
   });
 
-  it('should be able to create a new practitioner_qualification payload and validate with correct data', async () => {
-    const dataType: IPractitionerQualification = {
+  it('should be able to validate a new practitioner_qualification [createBackboneElement]', async () => {
+    const backboneElement = createBackboneElement('PractitionerQualification', {
       id: '123',
-      issuer: {
-        reference: 'test',
-      },
       code: {
         coding: [
           {
-            system: 'http://hl7.org/fhir/organization-qualification',
-            code: 'any',
+            code: '123',
+            system: 'system',
           },
         ],
       },
+      issuer: {
+        reference: 'Organization/123',
+      },
+    });
+
+    const validate = await Validator.backboneElements.PractitionerQualification(backboneElement);
+    expect(validate.isValid).toBeTruthy();
+    expect(validate.errors).toBeUndefined();
+  });
+
+  it('should be able to validate a new practitioner_qualification [new PractitionerQualification()]', async () => {
+    const item = new PractitionerQualification({
+      id: '123',
+      code: {
+        coding: [
+          {
+            code: '123',
+            system: 'system',
+          },
+        ],
+      },
+      issuer: {
+        reference: 'Organization/123',
+      },
+    });
+
+    const validateAddress = await Validator.backboneElements.PractitionerQualification(item);
+    expect(validateAddress.isValid).toBeTruthy();
+    expect(validateAddress.errors).toBeUndefined();
+  });
+
+  it('should be able to validate a new practitioner_qualification [IPractitionerQualification]', async () => {
+    const item: IPractitionerQualification = {
+      id: '123',
+      code: {
+        coding: [
+          {
+            code: '123',
+            system: 'system',
+          },
+        ],
+      },
+      issuer: {
+        reference: 'Organization/123',
+      },
     };
 
-    const validate = await validator.backboneElements.PractitionerQualification(dataType);
+    const validate = await Validator.backboneElements.PractitionerQualification(item);
 
     expect(validate.isValid).toBeTruthy();
     expect(validate.errors).toBeUndefined();
   });
 
-  it('should be able to validate a new practitioner_qualification payload and validate with wrong data', async () => {
-    const dataType = {
-      id: '123',
-      issuer: {
-        reference: 'test',
-      },
-      period: {
-        start: 'wrong date', // wrong date
-        end: '2023-01-01',
-      },
-      code: {
-        coding: [
-          {
-            system: 'http://hl7.org/fhir/organization-qualification',
-            code: 'any',
-          },
-        ],
-      },
-      wrongProperty: 'test', // wrong property
-    };
-
-    const validate = await validator.backboneElements.PractitionerQualification(dataType);
-
-    expect(validate.isValid).toBeFalsy();
-    expect(validate.errors).toBeDefined();
-    expect(validate.errors).toHaveLength(2);
-    expect(validate.errors).toEqual([
-      {
-        instancePath: '',
-        keyword: 'additionalProperties',
-        message: 'must NOT have additional properties',
-        params: { additionalProperty: 'wrongProperty' },
-        schemaPath: '#/additionalProperties',
-      },
-      {
-        instancePath: '/period/start',
-        keyword: 'pattern',
-        message: "The value '/period/start' does not match with datatype 'dateTime'",
-        params: { value: '/period/start' },
-        schemaPath: 'r4base.schema.json#/definitions/dateTime/pattern',
-      },
-    ]);
-  });
-
-  it('should be able to create a new practitioner_qualification payload using builder methods', async () => {
-    // build() is a method that returns the object that was built
-    const dataType = builder
+  it('should be able to create a new practitioner_qualification using builder methods [new PractitionerQualification()]', async () => {
+    const item = builder
       .setId('123')
       .setCode({
         coding: [
           {
-            system: 'http://hl7.org/fhir/organization-qualification',
-            code: 'any',
+            code: '123',
+            system: 'system',
           },
         ],
-        text: 'test',
       })
       .setIssuer({
-        reference: 'Organization/1',
+        reference: 'Organization/123',
       })
-      .setCode({
-        coding: [
-          {
-            system: 'http://hl7.org/fhir/organization-qualification',
-            code: 'any',
-          },
-        ],
-        text: 'test',
-      })
-      .addIdentifier({
-        use: 'official',
-        system: 'http://hl7.org/fhir/sid/us-npi',
-        value: '123',
+      .setPeriod({
+        start: '2020-01-01',
+        end: '2020-01-01',
       })
       .build();
 
-    expect(dataType).toBeDefined();
-    expect(dataType).toEqual({
+    expect(item).toEqual({
       code: {
         coding: [
           {
-            code: 'any',
-            system: 'http://hl7.org/fhir/organization-qualification',
+            code: '123',
+            system: 'system',
           },
         ],
-        text: 'test',
       },
       id: '123',
-      identifier: [
-        {
-          system: 'http://hl7.org/fhir/sid/us-npi',
-          use: 'official',
-          value: '123',
-        },
-      ],
       issuer: {
-        reference: 'Organization/1',
+        reference: 'Organization/123',
+      },
+      period: {
+        end: '2020-01-01',
+        start: '2020-01-01',
       },
     });
+  });
+
+  it('should be able to create a new address using builder methods [builders.dataTypes.AddressBuilder()]', async () => {
+    const item = builderFromFunction
+      .setId('123')
+      .setCode({
+        coding: [
+          {
+            code: '123',
+            system: 'system',
+          },
+        ],
+      })
+      .setIssuer({
+        reference: 'Organization/123',
+      })
+      .setPeriod({
+        start: '2020-01-01',
+        end: '2020-01-01',
+      })
+      .build();
+
+    expect(item).toEqual({
+      code: {
+        coding: [
+          {
+            code: '123',
+            system: 'system',
+          },
+        ],
+      },
+      id: '123',
+      issuer: {
+        reference: 'Organization/123',
+      },
+      period: {
+        end: '2020-01-01',
+        start: '2020-01-01',
+      },
+    });
+  });
+
+  it('should be get errors validators if new address has wrong data', async () => {
+    const item = {
+      id: '123',
+      wrongProperty: '123',
+    };
+
+    const validate = await Validator.backboneElements.PractitionerQualification(item);
+    expect(validate.isValid).toBeFalsy();
+    expect(validate.errors).toBeDefined();
+    expect(validate.errors?.length).toBe(2);
+    expect(validate.errors).toEqual([
+      {
+        instancePath: '',
+        keyword: 'required',
+        message: "must have required property 'code'",
+        params: {
+          missingProperty: 'code',
+        },
+        schemaPath: '#/required',
+      },
+      {
+        instancePath: '',
+        keyword: 'additionalProperties',
+        message: 'must NOT have additional properties',
+        params: {
+          additionalProperty: 'wrongProperty',
+        },
+        schemaPath: '#/additionalProperties',
+      },
+    ]);
   });
 });

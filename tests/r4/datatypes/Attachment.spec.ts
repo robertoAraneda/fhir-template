dc78a4bb2caff0d4afc8d3bfb991e69b09502ba8
Roberto Araneda Espinoza <robaraneda@gmail.com>
2023-06-11 19:00:20 -0400

    added unit test Attachment R4

--- tests/r4/datatypes/Attachment.spec.ts
+++ tests/r4/datatypes/Attachment.spec.ts
@@ -1,234 +1,151 @@
-import { IAttachment } from '../../../src/r4/interfaces/datatypes';
-import { IValidatorContext } from '../../../src/r4';
-import { AddressBuilder, AttachmentBuilder } from '../../../src/r4/builders/datatypes';
 import FHIRContext from '../../../src';
-import { Attachment } from '../../../src/r4/models/datatypes/Attachment';
+import { AttachmentBuilder } from '../../../src/r4/builders/datatypes';
+import { Attachment } from '../../../src/r4/models/datatypes';
+import { IAttachment } from '../../../src/r4/interfaces/datatypes';
 
-describe('Attachment', () => {
-  let validator: IValidatorContext;
+describe('Attachment FHIR R4', () => {
   let builder: AttachmentBuilder;
   let builderFromFunction: AttachmentBuilder;
-  const { validators: val, createDatatype, builders } = new FHIRContext().forR4();
-  validator = val;
+  const { Validator, createDatatype, Builder } = new FHIRContext().forR4();
 
   // create global
   beforeEach(() => {
     builder = new AttachmentBuilder();
-    builderFromFunction = builders.dataTypes.AttachmentBuilder();
+    builderFromFunction = Builder.dataTypes.AttachmentBuilder();
   });
 
-  it('should be able to create a new attachment and validate with correct data [createDatatype]', async () => {
-    const dataType = createDatatype('Attachment', {
+  it('should be able to validate a new attachment [createDatatype]', async () => {
+    const item = createDatatype('Attachment', {
       id: '123',
-      url: 'http://hl7.org/fhir/sid/us-npi',
-      contentType: 'test',
-      language: 'test',
-      data: 'test',
-      size: 1,
-      hash: 'test',
-      title: 'test',
-      creation: '2020-01-01',
-      width: 1,
-      height: 1,
-      frames: 1,
-      duration: 1,
-      pages: 1,
-      _creation: {
-        extension: [
-          {
-            url: 'test',
-            valueDate: '2020-01-01',
-          },
-        ],
-      },
+      url: 'http://example.com',
+      data: 'data',
+      title: 'title',
+      language: 'en',
+      contentType: 'application/pdf',
     });
 
-    const validate = await validator.dataTypes.Attachment(dataType);
-
+    const validate = await Validator.dataTypes.Attachment(item);
     expect(validate.isValid).toBeTruthy();
     expect(validate.errors).toBeUndefined();
   });
 
-  it('should be able to create a new attachment and validate with correct data [new Attachment()]', async () => {
-    const dataType = new Attachment({
+  it('should be able to validate a new attachment [new Attachment()]', async () => {
+    const item = new Attachment({
       id: '123',
-      url: 'http://hl7.org/fhir/sid/us-npi',
-      contentType: 'test',
-      language: 'test',
-      data: 'test',
-      size: 1,
-      hash: 'test',
-      title: 'test',
-      creation: '2020-01-01',
-      width: 1,
-      height: 1,
-      frames: 1,
-      duration: 1,
-      pages: 1,
-      _creation: {
-        extension: [
-          {
-            url: 'test',
-            valueDate: '2020-01-01',
-          },
-        ],
-      },
+      url: 'http://example.com',
+      data: 'data',
+      title: 'title',
+      language: 'en',
+      contentType: 'application/pdf',
     });
 
-    const validate = await validator.dataTypes.Attachment(dataType);
+    const validate = await Validator.dataTypes.Attachment(item);
+    expect(validate.isValid).toBeTruthy();
+    expect(validate.errors).toBeUndefined();
+  });
+
+  it('should be able to validate a new attachment [IAttachment]', async () => {
+    const item: IAttachment = {
+      id: '123',
+      url: 'http://example.com',
+      data: 'data',
+      title: 'title',
+      language: 'en',
+      contentType: 'application/pdf',
+    };
+
+    const validate = await Validator.dataTypes.Attachment(item);
 
     expect(validate.isValid).toBeTruthy();
     expect(validate.errors).toBeUndefined();
   });
 
-  it('should be able to create a new attachment and validate with correct data [IAttachment]', async () => {
-    const dataType: IAttachment = {
-      id: '123',
-      url: 'http://hl7.org/fhir/sid/us-npi',
-      contentType: 'test',
-      language: 'test',
-      data: 'test',
-      size: 1,
-      hash: 'test',
-      title: 'test',
-      creation: '2020-01-01',
-      width: 1,
-      height: 1,
-      frames: 1,
-      duration: 1,
-      pages: 1,
-      _creation: {
+  it('should be able to create a new attachment using builder methods [new Attachment()]', async () => {
+    const item = builder
+      .setId('123')
+      .setData('data')
+      .setContentType('application/pdf')
+      .setCreation('2020-01-01')
+      .setUrl('http://example.com')
+      .addParamExtension('pages', {
         extension: [
           {
-            url: 'test',
-            valueDate: '2020-01-01',
+            url: 'pages',
+            valueString: 'pages',
+          },
+        ],
+      })
+      .build();
+
+    expect(item).toEqual({
+      _pages: {
+        extension: [
+          {
+            url: 'pages',
+            valueString: 'pages',
           },
         ],
       },
-    };
-
-    const validate = await validator.dataTypes.Attachment(dataType);
-
-    expect(validate.isValid).toBeTruthy();
-    expect(validate.errors).toBeUndefined();
-  });
-
-  it('should be able to validate a new attachment and validate with wrong data', async () => {
-    const dataType = {
-      id: '123',
-      url: 'http://hl7.org/fhir/sid/us-npi',
-      contentType: 'test',
-      language: 'test',
-      data: 'test',
-      size: 1,
-      hash: 'test',
-      title: 'test',
-      creation: '2020',
-      width: 1,
-      test: 'test', // wrong property
-    };
-
-    const validate = await validator.dataTypes.Attachment(dataType);
-
-    expect(validate.isValid).toBeFalsy();
-    expect(validate.errors).toBeDefined();
-    expect(validate.errors).toHaveLength(1);
-    expect(validate.errors).toEqual([
-      {
-        instancePath: '',
-        schemaPath: '#/additionalProperties',
-        keyword: 'additionalProperties',
-        params: { additionalProperty: 'test' },
-        message: 'must NOT have additional properties',
-      },
-    ]);
+      contentType: 'application/pdf',
+      creation: '2020-01-01',
+      data: 'data',
+      url: 'http://example.com',
+    });
   });
 
   it('should be able to create a new attachment using builder methods [builders.dataTypes.AttachmentBuilder()]', async () => {
-    // build() is a method that returns the object that was built
-    const dataType = builderFromFunction
-      .setCreation('2020-01-01')
-      .setContentType('test')
-      .setData('test')
-      .setHash('test')
-      .setHeight(1)
+    const item = builderFromFunction
       .setId('123')
-      .setLanguage('test')
-      .setPages(1)
-      .setSize(1)
-      .addAttachmentParamExtension('pages', {
+      .setData('data')
+      .setContentType('application/pdf')
+      .setCreation('2020-01-01')
+      .setUrl('http://example.com')
+      .addParamExtension('pages', {
         extension: [
           {
-            url: 'test',
-            valueString: 'test',
+            url: 'pages',
+            valueString: 'pages',
           },
         ],
       })
       .build();
 
-    expect(dataType).toBeDefined();
-    expect(dataType).toEqual({
+    expect(item).toEqual({
       _pages: {
         extension: [
           {
-            url: 'test',
-            valueString: 'test',
+            url: 'pages',
+            valueString: 'pages',
           },
         ],
       },
-      contentType: 'test',
+      contentType: 'application/pdf',
       creation: '2020-01-01',
-      data: 'test',
-      hash: 'test',
-      height: 1,
-      id: '123',
-      language: 'test',
-      pages: 1,
-      size: 1,
+      data: 'data',
+      url: 'http://example.com',
     });
   });
 
-  it('should be able to create a new attachment using builder methods [new AttachmentBuilder()]', async () => {
-    // build() is a method that returns the object that was built
-    const dataType = builder
-      .setCreation('2020-01-01')
-      .setContentType('test')
-      .setData('test')
-      .setHash('test')
-      .setHeight(1)
-      .setId('123')
-      .setLanguage('test')
-      .setPages(1)
-      .setSize(1)
-      .addAttachmentParamExtension('pages', {
-        extension: [
-          {
-            url: 'test',
-            valueString: 'test',
-          },
-        ],
-      })
-      .build();
-
-    expect(dataType).toBeDefined();
-    expect(dataType).toEqual({
-      _pages: {
-        extension: [
-          {
-            url: 'test',
-            valueString: 'test',
-          },
-        ],
-      },
-      contentType: 'test',
-      creation: '2020-01-01',
-      data: 'test',
-      hash: 'test',
-      height: 1,
+  it('should be get errors validators if new attachment has wrong data', async () => {
+    const item = {
       id: '123',
-      language: 'test',
-      pages: 1,
-      size: 1,
-    });
+      wrongProperty: 'wrong',
+    };
+
+    const validate = await Validator.dataTypes.Attachment(item);
+    expect(validate.isValid).toBeFalsy();
+    expect(validate.errors).toBeDefined();
+    expect(validate.errors?.length).toBe(1);
+    expect(validate.errors).toEqual([
+      {
+        instancePath: '',
+        keyword: 'additionalProperties',
+        message: 'must NOT have additional properties',
+        params: {
+          additionalProperty: 'wrongProperty',
+        },
+        schemaPath: '#/additionalProperties',
+      },
+    ]);
   });
 });

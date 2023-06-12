dddf151b0a4cdedb55ff9f1d76a339077ac1dc3d
Roberto Araneda Espinoza <robaraneda@gmail.com>
2023-06-11 20:56:19 -0400

    added unit test PractitionerRole Resource R4

--- tests/r4/resources/PractitionerRole.spec.ts
+++ tests/r4/resources/PractitionerRole.spec.ts
@@ -1,20 +1,22 @@
 import { PractitionerRoleBuilder } from '../../../src/r4/builders/resources';
 import { IPractitionerRole } from '../../../src/r4/interfaces/resources';
-import { IValidatorContext } from '../../../src/r4';
 import FHIRContext from '../../../src';
+import { PractitionerRole } from '../../../src/r4/models/resources';
 
-describe('PractitionerRole', () => {
-  const context = new FHIRContext();
-  let validator = context.forR4().validators.resources;
+describe('PractitionerRole Resource FHIR R4', () => {
   let builder: PractitionerRoleBuilder;
+  let builderFromFunction: PractitionerRoleBuilder;
+  const context = new FHIRContext();
+  const { Validator, Builder, createResource } = context.forR4();
 
   // create global
   beforeEach(() => {
     builder = new PractitionerRoleBuilder();
+    builderFromFunction = Builder.resources.PractitionerRole();
   });
 
-  it('should be able to create a new practitioner role and validate with correct data [Example PractitionerRole/example]', async () => {
-    const dataType: IPractitionerRole = {
+  it('should be able to create a new practitioner role and validate with correct data [IPractitionerRole]', async () => {
+    const item: IPractitionerRole = {
       resourceType: 'PractitionerRole',
       id: 'example',
       text: {
@@ -79,74 +81,28 @@ describe('PractitionerRole', () => {
           reference: 'HealthcareService/example',
         },
       ],
-      contact: [
+      availableTime: [
         {
-          telecom: [
-            {
-              system: 'phone',
-              value: '(03) 5555 6473',
-              use: 'work',
-            },
-            {
-              system: 'email',
-              value: 'adam.southern@example.org',
-              use: 'work',
-            },
-          ],
+          daysOfWeek: ['mon', 'tue', 'wed'],
+          availableStartTime: '09:00:00',
+          availableEndTime: '16:30:00',
+        },
+        {
+          daysOfWeek: ['thu', 'fri'],
+          availableStartTime: '09:00:00',
+          availableEndTime: '12:00:00',
         },
       ],
-      characteristic: [
+      notAvailable: [
         {
-          coding: [
-            {
-              system: 'http://hl7.org/fhir/service-mode',
-              code: 'in-person',
-              display: 'In Person',
-            },
-            {
-              system: 'http://hl7.org/fhir/service-mode',
-              code: 'videoconference',
-              display: 'Video Conference',
-            },
-          ],
+          description: 'Adam will be on extended leave during May 2017',
+          during: {
+            start: '2017-05-01',
+            end: '2017-05-20',
+          },
         },
-      ],
-      communication: [
         {
-          coding: [
-            {
-              system: 'urn:ietf:bcp:47',
-              code: 'en',
-            },
-          ],
-        },
-      ],
-      availability: [
-        {
-          availableTime: [
-            {
-              daysOfWeek: ['mon', 'tue', 'wed'],
-              availableStartTime: '09:00:00',
-              availableEndTime: '16:30:00',
-            },
-            {
-              daysOfWeek: ['thu', 'fri'],
-              availableStartTime: '09:00:00',
-              availableEndTime: '12:00:00',
-            },
-          ],
-          notAvailableTime: [
-            {
-              description: 'Adam will be on extended leave during May 2017',
-              during: {
-                start: '2017-05-01',
-                end: '2017-05-20',
-              },
-            },
-            {
-              description: 'Adam is generally unavailable on public holidays and during the Christmas/New Year break',
-            },
-          ],
+          description: 'Adam is generally unavailable on public holidays and during the Christmas/New Year break',
         },
       ],
       endpoint: [
@@ -156,14 +112,14 @@ describe('PractitionerRole', () => {
       ],
     };
 
-    const validate = await validator.PractitionerRole(dataType);
+    const validate = await Validator.resources.PractitionerRole(item);
 
     expect(validate.isValid).toBeTruthy();
     expect(validate.errors).toBeUndefined();
   });
 
-  it('should be able to validate a new contact point and validate with wrong data [Example PractitionerRole/3ad0687e-f477-468c-afd5-fcc2bf897808]', async () => {
-    const dataType: IPractitionerRole = {
+  it('should be able to validate a new contact point and validate with wrong data [new PractitionerRole()]', async () => {
+    const item = new PractitionerRole({
       resourceType: 'PractitionerRole',
       id: 'f007-0',
       text: {
@@ -200,17 +156,63 @@ describe('PractitionerRole', () => {
           ],
         },
       ],
-    };
+    });
 
-    const validate = await validator.PractitionerRole(dataType);
+    const validate = await Validator.resources.PractitionerRole(item);
 
     expect(validate.isValid).toBeTruthy();
     expect(validate.errors).toBeUndefined();
   });
 
-  it('should be able to create a new contact point using builder methods', async () => {
+  it('should be able to validate a new contact point and validate with wrong data [createResource()]', async () => {
+    const item = createResource('PractitionerRole', {
+      resourceType: 'PractitionerRole',
+      id: 'f007-0',
+      text: {
+        status: 'generated',
+        div: '<div xmlns="http://www.w3.org/1999/xhtml">Generated</div>',
+      },
+      practitioner: {
+        reference: 'Practitioner/f007',
+        display: 'Simone Heps',
+      },
+      organization: {
+        reference: 'Organization/f001',
+        display: 'BMC',
+      },
+      code: [
+        {
+          coding: [
+            {
+              system: 'http://snomed.info/sct',
+              code: '59058001',
+              display: 'General physician',
+            },
+          ],
+        },
+      ],
+      specialty: [
+        {
+          coding: [
+            {
+              system: 'http://snomed.info/sct',
+              code: '394814009',
+              display: 'General practice',
+            },
+          ],
+        },
+      ],
+    });
+
+    const validate = await Validator.resources.PractitionerRole(item);
+
+    expect(validate.isValid).toBeTruthy();
+    expect(validate.errors).toBeUndefined();
+  });
+
+  it('should be able to create a new contact point using builder methods [new PractitionerRoleBuilder()]', async () => {
     // build() is a method that returns the object that was built
-    const dataType = builder
+    const item = builder
       .addCode({
         coding: [
           {
@@ -219,7 +221,7 @@ describe('PractitionerRole', () => {
           },
         ],
       })
-      .addPractitionerRoleParamExtension('active', {
+      .addParamExtension('active', {
         extension: [
           {
             url: 'active',
@@ -233,8 +235,63 @@ describe('PractitionerRole', () => {
       })
       .build();
 
-    expect(dataType).toBeDefined();
-    expect(dataType).toEqual({
+    expect(item).toBeDefined();
+    expect(item).toEqual({
+      _active: {
+        extension: [
+          {
+            url: 'active',
+            valueBoolean: true,
+          },
+        ],
+      },
+      active: true,
+      code: [
+        {
+          coding: [
+            {
+              code: '59058001',
+              system: 'http://snomed.info/sct',
+            },
+          ],
+        },
+      ],
+      healthcareService: [
+        {
+          reference: 'HealthcareService/example',
+        },
+      ],
+      resourceType: 'PractitionerRole',
+    });
+  });
+
+  it('should be able to create a new contact point using builder methods [Builder.resources.PractitionerRole()]', async () => {
+    // build() is a method that returns the object that was built
+    const item = builderFromFunction
+      .addCode({
+        coding: [
+          {
+            system: 'http://snomed.info/sct',
+            code: '59058001',
+          },
+        ],
+      })
+      .addParamExtension('active', {
+        extension: [
+          {
+            url: 'active',
+            valueBoolean: true,
+          },
+        ],
+      })
+      .setActive(true)
+      .addHealthcareService({
+        reference: 'HealthcareService/example',
+      })
+      .build();
+
+    expect(item).toBeDefined();
+    expect(item).toEqual({
       _active: {
         extension: [
           {

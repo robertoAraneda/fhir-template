8c77e52ad0792eab3e60e27edbe854effeb61c57
Roberto Araneda Espinoza <robaraneda@gmail.com>
2023-06-12 11:30:32 -0400

    added unit test PatientLink Resource R5

--- tests/r5/backbones/PatientLink.spec.ts
+++ tests/r5/backbones/PatientLink.spec.ts
@@ -1,23 +1,21 @@
-import { PatientLinkBuilder } from '../../../src/r5/builders/backbones';
+import { PatientContactBuilder, PatientLinkBuilder } from '../../../src/r5/builders/backbones';
 import { IPatientLink } from '../../../src/r5/interfaces/backbones';
 import { IValidatorContext } from '../../../src/r5';
 import FHIRContext from '../../../src';
+import { PatientLink } from '../../../src/r5/models/backbones/PatientLink';
 
-describe('PatientLink', () => {
-  let validator: IValidatorContext;
+describe('PatientLink FHIR R5', () => {
+  const { Validator, Builder, createBackboneElement } = new FHIRContext().forR5();
   let builder: PatientLinkBuilder;
-
-  beforeAll(() => {
-    const context = new FHIRContext();
-    validator = context.forR5().validators;
-  });
+  let builderFromFunction: PatientLinkBuilder;
 
   // create global
   beforeEach(() => {
     builder = new PatientLinkBuilder();
+    builderFromFunction = Builder.backboneElements.PatientLink();
   });
 
-  it('should be able to create a new patient_link payload and validate with correct data', async () => {
+  it('should be able to create a new patient_link payload and validate with correct data [IPatientLink]', async () => {
     const dataType: IPatientLink = {
       id: '123',
       other: {
@@ -32,7 +30,49 @@ describe('PatientLink', () => {
       ],
     };
 
-    const validate = await validator.backboneElements.PatientLink(dataType);
+    const validate = await Validator.backboneElements.PatientLink(dataType);
+
+    expect(validate.isValid).toBeTruthy();
+    expect(validate.errors).toBeUndefined();
+  });
+
+  it('should be able to create a new patient_link payload and validate with correct data [new PatientLink()]', async () => {
+    const dataType = new PatientLink({
+      id: '123',
+      other: {
+        reference: 'test',
+      },
+      type: 'replaced-by', // correct type
+      extension: [
+        {
+          url: 'test',
+          valueString: 'test',
+        },
+      ],
+    });
+
+    const validate = await Validator.backboneElements.PatientLink(dataType);
+
+    expect(validate.isValid).toBeTruthy();
+    expect(validate.errors).toBeUndefined();
+  });
+
+  it('should be able to create a new patient_link payload and validate with correct data', async () => {
+    const dataType = createBackboneElement('PatientLink', {
+      id: '123',
+      other: {
+        reference: 'test',
+      },
+      type: 'replaced-by', // correct type
+      extension: [
+        {
+          url: 'test',
+          valueString: 'test',
+        },
+      ],
+    });
+
+    const validate = await Validator.backboneElements.PatientLink(dataType);
 
     expect(validate.isValid).toBeTruthy();
     expect(validate.errors).toBeUndefined();
@@ -50,7 +90,7 @@ describe('PatientLink', () => {
       wrongProperty: 'test', // wrong property
     };
 
-    const validate = await validator.backboneElements.PatientLink(dataType);
+    const validate = await Validator.backboneElements.PatientLink(dataType);
 
     expect(validate.isValid).toBeFalsy();
     expect(validate.errors).toBeDefined();
@@ -80,9 +120,45 @@ describe('PatientLink', () => {
     ]);
   });
 
-  it('should be able to create a new patient_link payload using builder methods', async () => {
+  it('should be able to create a new patient_link payload using builder methods [new PatientLinkBuilder()]', async () => {
     // build() is a method that returns the object that was built
     const dataType = builder
+      .addPatientLinkParamExtension('type', {
+        extension: [
+          {
+            url: 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
+            valueString: 'Jane Doe',
+          },
+        ],
+      })
+      .addExtension({
+        url: 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
+        valueString: 'Jane Doe',
+      })
+      .build();
+
+    expect(dataType).toBeDefined();
+    expect(dataType).toEqual({
+      _type: {
+        extension: [
+          {
+            url: 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
+            valueString: 'Jane Doe',
+          },
+        ],
+      },
+      extension: [
+        {
+          url: 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
+          valueString: 'Jane Doe',
+        },
+      ],
+    });
+  });
+
+  it('should be able to create a new patient_link payload using builder methods [Builder.backboneElements.PatientLink()]', async () => {
+    // build() is a method that returns the object that was built
+    const dataType = builderFromFunction
       .addPatientLinkParamExtension('type', {
         extension: [
           {
